ba366b5c271c7cf50ca66d31afeb74460c523372
chipperrip <36035495+chipperrip@users.noreply.github.com>
2018-02-01 23:33:43 +0100

    Week7 task 1

--- mooc-2013-OOProgrammingWithJava-PART2/week7-week7_01.Smileys/src/Smileys.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week7-week7_01.Smileys/src/Smileys.java
@@ -2,10 +2,43 @@
 public class Smileys {
 
     public static void main(String[] args) {
-        // Test your method at least with the following
-        // printWithSmileys("Method");
-        // printWithSmileys("Beerbottle");
-        // printWithSmileys("Interface");
+        //   Test your method at least with the following
+        printWithSmileys("Method");
+        printWithSmileys("Beerbottle");
+        printWithSmileys("Interface");
     }
 
+    private static void printWithSmileys(String characterString) {
+
+        //if the string is of odd length, add a whitespace " "
+        if (characterString.length() % 2 == 1) {
+            characterString += " ";
+        }
+
+        // prefix and suffix string with whitespaces 
+        characterString = " " + characterString + " ";
+
+        //surround string with a box of smileys
+        System.out.println(smileyRow(characterString.length()));
+        System.out.println(smiley() + characterString + smiley());
+        System.out.println(smileyRow(characterString.length()));
+
+    }
+
+    private static String smileyRow(int length) {
+
+        //prints a row of smileys relative to the length of the supplied string
+        String smileyRow = "";
+
+        for (int i = 0; i < (length / 2) + 2; i++) {
+            smileyRow += smiley();
+        }
+        return smileyRow;
+    }
+
+    private static String smiley() {
+
+        //supplies smileys wherever they might be needed
+        return ":)";
+    }
 }
