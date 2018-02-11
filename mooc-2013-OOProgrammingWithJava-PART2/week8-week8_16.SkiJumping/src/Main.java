1894096fa89bd01638dbf1a0953e26b8478cd268
chipperrip <36035495+chipperrip@users.noreply.github.com>
2018-02-11 23:55:19 +0100

    tstdawd

--- mooc-2013-OOProgrammingWithJava-PART2/week8-week8_16.SkiJumping/src/Main.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week8-week8_16.SkiJumping/src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         // Write your main program here. Implementing your own classes will be very useful.
         
-        int test = 0;
-        int poopts = 23;
+        
     }
 }
