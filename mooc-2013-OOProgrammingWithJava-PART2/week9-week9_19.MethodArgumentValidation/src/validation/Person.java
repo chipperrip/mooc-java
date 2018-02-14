4e2a62c9c383666f8df4bc9c11ef728a73875bcb
chipperrip <36035495+chipperrip@users.noreply.github.com>
2018-02-14 12:00:37 +0100

    19

--- mooc-2013-OOProgrammingWithJava-PART2/week9-week9_19.MethodArgumentValidation/src/validation/Person.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week9-week9_19.MethodArgumentValidation/src/validation/Person.java
@@ -7,6 +7,15 @@ public class Person {
 
     public Person(String name, int age) {
 
+        if (name == null
+                || name.length() > 39
+                || name.isEmpty()
+                || (age >= 0 && age <= 120)) {
+            throw new IllegalArgumentException(
+                    "Name variable must not be null, "
+                    + "empty, or longer than 40 characters. "
+                    + "The age has also to be between 0-120. ");
+        }
         this.name = name;
         this.age = age;
     }
